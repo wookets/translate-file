f5088e0b6b0f7eed4e1e8c6aed73d15cec73b004
Sean Wesenberg <wookets@gmail.com>
2018-07-21 16:45:22 -0500

    Added streams for more efficient processing

--- src/translate-file.js
+++ src/translate-file.js
@@ -1,11 +1,28 @@
 
 const fs = require('fs')
+const through2 = require('through2')
 const translate = require('./translate')
 
 async function translateFile (inFile, outFile, targetLanguage) {
-	const json = fs.readFileSync(inFile, 'utf8')
-	const output = await translate(JSON.stringify(json), 'en')
-	fs.writeFileSync(outFile, output)
+	let count = 0
+	const inStream = fs.createReadStream(inFile, { highWaterMark: 1024 })
+	const outStream = fs.createWriteStream(outFile)
+	inStream
+		.pipe(through2(function (chunk, enc, callback) {
+			count = count + 1
+			process.stdout.write(`translating chunk ${count}... `)
+			translate(chunk.toString(), 'en')
+				.then( result => {
+					process.stdout.write(`writing chunk ${count} to file \n`)
+					this.push(result)
+					setTimeout(callback, 100)
+				})
+				.catch(console.log)
+		}))
+		.pipe(outStream)
+		.on('finish', () => {
+			console.log('All done!')
+		})
 }
 
 module.exports = translateFile
